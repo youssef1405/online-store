62e85acc7e137a8bcd7fab321c13d14799331f40
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-09-18 22:34:03 -0400

    display all products data on the main page

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent {
+  @Input() product: Product;
 
+  constructor() {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: '',
+    };
+  }
 }
